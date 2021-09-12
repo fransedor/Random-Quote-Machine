f9a1a415ccbcbfadd2f2d49570d4c2d7cb49dcaa
fransedor <fransedoravelino@gmail.com>
2021-09-12 09:38:44 +0700

    tweet and new quote functionality done

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [text, setText] = useState('');
+  const [author, setAuthor] = useState('');
+  const [link,setLink] = useState('');
+
+  function getQuote() {
+    const quote = fetch('https://api.quotable.io/random')
+    .then(response => response.json())
+    .then(data => {
+      setText(data.content);
+      setAuthor(data.author);
+      setLink('https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=' + encodeURIComponent('"' + data.content + '" ' + data.author))
+    });
+
+}
+  useEffect(() => {
+    getQuote();
+  },[]);
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="wrapper" id="quote-box">
+        <p id="text">{text}</p>
+        <p id="author">{author}</p>
+        <div className="buttons">
+          <a href={link} id="tweet-quote" target="_blank"><img src="https://cdn-icons-png.flaticon.com/512/733/733635.png" alt="Tweet" /></a>
+          <button id="new-quote" onClick={getQuote}>New Quote</button>
+        </div>
+      </div>
     </div>
   );
 }
